e7040f186c3a5680444d65546f3eff87018ee73c
Tadeas Kriz <tadeas@brightify.org>
2016-03-02 14:36:31 +0100

    Add Carthage support.

---
+++ M13Checkbox/M13Checkbox.h
@@ -0,0 +1,20 @@
+//
+//  M13Checkbox.h
+//  M13Checkbox
+//
+//  Created by Tadeas Kriz on 02/03/16.
+//  Copyright © 2016 Brandon McQuilkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for M13Checkbox.
+FOUNDATION_EXPORT double M13CheckboxVersionNumber;
+
+//! Project version string for M13Checkbox.
+FOUNDATION_EXPORT const unsigned char M13CheckboxVersionString[];
+
+#import <M13Checkbox/M13CheckboxView.h>
+// In this header, you should import all the public headers of your framework using statements like #import <M13Checkbox/PublicHeader.h>
+
+
